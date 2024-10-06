d075ecf74877a13276831f49b42cd262bcc7507b
fraddy <ssisithasiranga@gmail.com>
2024-10-06 21:18:21 +0530

    create base

---
+++ src/main/java/com/fraddy/goldenbanana/TheGoldenBananaApplication.java
@@ -0,0 +1,13 @@
+package com.fraddy.goldenbanana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheGoldenBananaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheGoldenBananaApplication.class, args);
+    }
+
+}
